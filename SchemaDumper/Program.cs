da736a4a6bb6264321ac698a8cbfb4c95c01d6ee
Giovanni Bassi <giggio@giggio.net>
2015-02-25 20:31:47 -0300

    get it to work

---
+++ SchemaDumper/Program.cs
@@ -0,0 +1,70 @@
+﻿using DocoptNet;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Generators.SqlServer;
+using FluentMigrator.Runner.Processors;
+using FluentMigrator.Runner.Processors.SqlServer;
+using FluentMigrator.SchemaDump.SchemaDumpers;
+using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using static System.Console;
+
+namespace SchemaDumper
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            const string usage = @"Fluent Migrator Schema Dumper
+  Usage:
+    SchemaDumper.exe --connection CONNECTION [--file FILE] [--verbose] [--show] [--open]
+    SchemaDumper.exe --version
+    SchemaDumper.exe --help
+
+  Options:
+    --connection CONNECTION -c CONNECTION    The connection string. Required.
+    --file FILE -f FILE                      File to output. Optional. [default: schemaDump.cs]
+    --show -s                                Show output. Optional.
+    --open -o                                Open file. Optional.
+    --verbose                                Verbose. Optional.
+    --help -h                                Show this screen.
+    --version -v                             Show version.
+";
+
+            var arguments = new Docopt().Apply(usage, args, version: Assembly.GetExecutingAssembly().GetName().Version, exit: true);
+            var file = arguments["--file"].ToString();
+            var verbose = arguments["--verbose"].IsTrue;
+            var open = arguments["--open"].IsTrue;
+            var show = arguments["--show"].IsTrue;
+            if (!Path.IsPathRooted(file)) file = Path.Combine(Environment.CurrentDirectory, file);
+            var connectionString = arguments["--connection"].ToString();
+            if (verbose) WriteLine($"Saving to {file}.");
+            try { var builder = new SqlConnectionStringBuilder(connectionString); }
+            catch (ArgumentException)
+            {
+                WriteLine("Connection string is in incorrect format.");
+                return;
+            }
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try { connection.Open(); }
+                catch (SqlException ex)
+                {
+                    WriteLine($"Connection couldn't be established:\n{ex.Message}");
+                    return;
+                }
+                var consoleAnnouncer = new ConsoleAnnouncer();
+                var dumper = new SqlServerSchemaDumper(new SqlServerProcessor(connection, new SqlServer2000Generator(), consoleAnnouncer, new ProcessorOptions(), new SqlServerDbFactory()), consoleAnnouncer);
+                var tables = dumper.ReadDbSchema();
+                var writer = new RCDumpWriter();
+                writer.WriteToFile(tables, file);
+            }
+            if (show) WriteLine(File.ReadAllText(file));
+            if (open) try { Process.Start(file); } catch { }
+            if (verbose) WriteLine("Done.");
+        }
+    }
+}
---
+++ SchemaDumper/RCDumpWriter.cs
@@ -0,0 +1,102 @@
+﻿//as per https://gist.github.com/rebootd/924680#file-sqlschemadumpwriter
+using FluentMigrator.Model;
+using FluentMigrator.SchemaDump.SchemaWriters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+
+namespace SchemaDumper
+{
+    public class RCDumpWriter : SchemaWriterBase
+    {
+        public override void WriteToStream(ICollection<TableDefinition> tables, StreamWriter output)
+        {
+            //int tableCount = tables.Count;
+            //int columnCount = tables.Select(t => t.Columns.Count).Sum();
+            //int indexCount = tables.Select(t => t.Indexes.Count).Sum();
+            //int keyCount = tables.Select(t => t.ForeignKeys.Count).Sum();
+
+            //start writing a migration file
+            output.WriteLine("using System;");
+            output.WriteLine("using System.Collections.Generic;");
+            output.WriteLine("using System.Linq;");
+            output.WriteLine("using System.Web;");
+            output.WriteLine("using FluentMigrator;");
+            output.WriteLine(string.Empty);
+            output.WriteLine("namespace MyAppNamespace.Migrations");
+            output.WriteLine("{");
+            output.WriteLine("\t[Migration(0)]");
+            output.WriteLine("\tpublic class BaseMigration : Migration");
+            output.WriteLine("\t{");
+            output.WriteLine("\t\tpublic override void Up()");
+            output.WriteLine("\t\t{");
+
+            foreach (TableDefinition table in tables)
+            {
+                WriteTable(table, output);
+            }
+
+            output.WriteLine("\t\t}"); //end method
+
+            output.WriteLine("\t\tpublic override void Down()");
+            output.WriteLine("\t\t{");
+
+            foreach (TableDefinition table in tables)
+            {
+                WriteDeleteTable(table, output);
+            }
+
+            output.WriteLine("\t\t}"); //end method
+            output.WriteLine("\t}"); //end class
+            output.WriteLine(String.Empty);
+            output.WriteLine("}"); //end namespace
+        }
+
+        protected void WriteTable(TableDefinition table, StreamWriter output)
+        {
+            output.WriteLine("\t\t\tCreate.Table(\"" + table.Name + "\")");
+            foreach (ColumnDefinition column in table.Columns)
+            {
+                WriteColumn(column, output, column == table.Columns.Last());
+            }
+        }
+
+        protected void WriteDeleteTable(TableDefinition table, StreamWriter output)
+        {
+            //Delete.Table("Bar");
+            output.WriteLine("\t\t\tDelete.Table(\"" + table.Name + "\");");
+        }
+
+        protected void WriteColumn(ColumnDefinition column, StreamWriter output, bool isLastColumn)
+        {
+            string columnSyntax = ".WithColumn(\"" + column.Name + "\")";
+            switch (column.Type)
+            {
+                case DbType.Boolean:
+                    columnSyntax += ".AsBoolean()";
+                    break;
+                case DbType.Int16:
+                    columnSyntax += ".AsInt16()";
+                    break;
+                case DbType.Int32:
+                    columnSyntax += ".AsInt32()";
+                    break;
+                default:
+                    columnSyntax += ".AsString()";
+                    break;
+            }
+            if (column.IsIdentity)
+                columnSyntax += ".Identity()";
+            else if (column.IsIndexed)
+                columnSyntax += ".Indexed()";
+
+            if (column.IsNullable == false)
+                columnSyntax += ".NotNullable()";
+
+            if (isLastColumn) columnSyntax += ";";
+            output.WriteLine("\t\t\t\t" + columnSyntax);
+        }
+    }
+}
